f12f078be6fdb015c001980b37796433007d095b
yacjoznau <168394911+yacjoznau@users.noreply.github.com>
2024-05-18 11:55:58 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -39,9 +39,17 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+speed_x = 3
+speed_y = 3
+
 RAKETKA1 = Player(player_image='racket.png', player_x=30, player_y=200, player_speed=4, wight=50, height=150)
 RAKETKA2 = Player(player_image='racket.png', player_x=520, player_y=200, player_speed=4, wight=50, height=150)
 ball = GameSprite(player_image='ball.png', player_x=200, player_y=200, player_speed=4, wight=50, height=50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 while game:
     for e in event.get():
@@ -49,9 +57,28 @@ while game:
             game = False
 
     if finish != True:
+
         window.fill(back)
         RAKETKA1.update_l()
         RAKETKA2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(RAKETKA1, ball) or sprite.collide_rect(RAKETKA2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+
         RAKETKA1.reset()
         RAKETKA2.reset()
         ball.reset()
